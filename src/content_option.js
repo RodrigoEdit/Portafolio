eb13c355b29622d0e418078bc08004dd6a002356
Rodrigo_Elguera <111022228+RodrigoEdit@users.noreply.github.com>
2023-11-14 23:35:16 +0000

    commit

---
+++ src/content_option.js
@@ -0,0 +1,107 @@
+const logotext = "Rodrigo";
+const meta = {
+    title: "Rodrigo Elguera",
+    description: "I’m Rodrigo Elguera data scientist _ Full stack devloper and mobile app developer",
+};
+
+const introdata = {
+    title: "I'm Rodrigo Elguera",
+    animated: {
+        first: "I love coding",
+        second: "I code websites",
+        third: "I develop mobile apps",
+    },
+    description: "I am a passionate programmer with experience in developing web and mobile applications. I have knowledge of Html5, Css3, Bootstrap, Python, Java, C#, SQL Server and MySQL. My focus is on providing efficient and high-quality solutions. Worked on automated systems projects and inventory management systems. I have strong skills and a quick learning ability. I am a proactive and communicative collaborator, always keeping up to date with the latest technologies. My goal is to create applications that provide user convenience with quick and easy interactions.",
+    your_img_url: "https://i.pinimg.com/originals/82/e2/58/82e25859f382250e9f609f8d79c73ed0.png",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Graduated from IDAT and completed courses on Udemy and FriendsCode, I am a dedicated learner with a strong foundation in programming. Experienced in both theoretical knowledge and practical application, I bring a versatile skill set to every project. Let's collaborate to create impactful solutions.",
+};
+const worktimeline = [{
+        jobtitle: "practicing",
+        where: "La Huaca",
+        date: "2023",
+    },
+    {
+        jobtitle: "",
+        where: "",
+        date: "",
+    },
+];
+
+const skills = [{
+        name: "Jquery - java - Javascript",
+        value: 70,
+    },
+    {
+        name: "C++ - C# - Python",
+        value: 50,
+    },
+    {
+        name: "SQL server - Mysql - PostgreSql",
+        value: 60,
+    },
+    {
+        name: "ReactJS",
+        value: 70,
+    },
+];
+
+const services = [{
+        title: "BackEnd",
+        description: "Experienced backend developer specializing in Java and Spring Boot. Proficient in building scalable and efficient server-side applications. Passionate about designing and implementing robust APIs and database systems. Let's collaborate to create powerful and reliable backend solutions for your projects.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Experienced mobile app developer proficient in Java and Kotlin. Skilled at creating robust and user-friendly applications for Android devices. Passionate about delivering high-quality and innovative solutions. Let's turn your app ideas into reality!",
+    },
+    {
+        title: "FrontEnd Design",
+        description: "Skilled Frontend developer specializing in ReactJS and Angular frameworks. Experienced in creating dynamic and interactive user interfaces. Passionate about delivering visually appealing and responsive web applications. Let's work together to bring your frontend vision to life with cutting-edge technologies.",
+    },
+    {
+        title: "Data Base",
+        description: "Experienced database developer proficient in SQL Server, MySQL, and SQLite. Skilled at designing and optimizing database structures and writing efficient queries. Passionate about ensuring data integrity and performance. Let's collaborate to create robust and scalable database solutions for your applications.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://i.pinimg.com/736x/32/e1/b3/32e1b381b0a7008424ec1b21dd229826.jpg",
+        description: "Hotel AURILI",
+        link: "http://elgueraproyectos.site/index_auliri.html",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "elgueraamadorodrigo@gmail.com",
+    YOUR_FONE: "(51)933534235",
+    description: "Interested in my services? Feel free to contact me! I'm available for web and mobile development projects. With my experience in multiple programming languages and creating efficient applications, I can help take your project to the next level. Get in touch and let's work together to create innovative and high-quality solutions!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_87jyyuj",
+    YOUR_TEMPLATE_ID: "template_eh41exp",
+    YOUR_USER_ID: "qLsL-MiVnvRXCN8Rr", //PUBLIC KEY
+};
+
+const socialprofils = {
+    github: "https://github.com/RodrigoEdit",
+    facebook: "https://www.facebook.com/rodrigoelgueraxd/",
+    linkedin: "https://www.linkedin.com/in/rodrigo-elguera-amado/",
+    twitter: "https://twitter.com/ElgueraRod09",
+};
+
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
